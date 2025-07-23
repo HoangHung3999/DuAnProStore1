7e1422c24a35253b9fb150f72d4d64d92d72154a
sonhoang-git <sonhaph40315@fpt.edu.vn>
2025-07-23 11:40:07 +0700

    Thiết kế màn giỏ hàng ,bottomNavigation

--- app/src/main/java/fpoly/hungph53757/duanprostore/MainActivity.java
+++ app/src/main/java/fpoly/hungph53757/duanprostore/MainActivity.java
@@ -7,19 +7,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import fpoly.hungph53757.duanprostore.BottomNavigationFragment.Home_Fragment;
+import fpoly.hungph53757.duanprostore.BottomNavigationFragment.ShoppingCart_Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment cartFragment = new ShoppingCart_Fragment();
+    Fragment homeFragment = new Home_Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(item ->{
+            int id = item.getItemId();
+            if(id == R.id.item_home){
+                loadFragment(homeFragment);
+                return true;
+            } else if (id==R.id.item_cart) {
+                loadFragment(cartFragment);
+                return true;
+            }
+            return false;
         });
-        //bbbbbbb
+    }
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
     }
 }
+
+
